5985bb207528a1b47dc31c305930bf9872c41019
DarkPromise <hazard7@live.com.sg>
2015-08-18 09:11:15 +0800

    Edited Tile Contructor
    Added Member Variables:
    m_bActive
    m_bCollidable
    Added get set functions for member variables
    
    Added TILE_ID Enum

--- FRAMEWURK/Source/Tile.cpp
+++ FRAMEWURK/Source/Tile.cpp
@@ -1,11 +1,58 @@
 #include "Tile.h"
 
-
-Tile::Tile(void)
+Tile::Tile(TILE_ID tileID) : m_bActive(true),
+	m_bCollidable(false)
 {
+	this->m_eTileID = tileID;
 }
-
 
 Tile::~Tile(void)
 {
 }
+
+bool Tile::isActive()
+{
+	return this->m_bActive;
+}
+
+void Tile::setActive()
+{
+	if(this->m_bActive)
+	{
+		m_bActive = true;
+	}
+	else
+	{
+		m_bActive = false;
+	}
+}
+
+bool Tile::isCollidable()
+{
+	return this->m_bCollidable;
+}
+
+void Tile::setCollidable()
+{
+	if(this->m_bCollidable)
+	{
+		m_bCollidable = true;
+	}
+	else
+	{
+		m_bCollidable = false;
+	}
+}
+
+TILE_ID Tile::getTileID()
+{
+	return this->m_eTileID;
+}
+
+void Tile::setTileID(TILE_ID tileID)
+{
+	this->m_eTileID = tileID;
+}
+
+
+
--- FRAMEWURK/Source/Tile.h
+++ FRAMEWURK/Source/Tile.h
@@ -1,8 +1,30 @@
-#pragma once
+#ifndef TILE_H
+#define TILE_H
+
+enum TILE_ID
+{
+	Tile1 = 0,
+};
+
 class Tile
 {
 public:
-	Tile(void);
+	Tile(TILE_ID tileID);
 	~Tile(void);
+
+	bool isActive();
+	void setActive();
+
+	bool isCollidable();
+	void setCollidable();
+
+	TILE_ID getTileID();
+	void setTileID(TILE_ID tileID);
+
+private:
+	TILE_ID m_eTileID;
+	bool m_bActive;
+	bool m_bCollidable;
 };
 
+#endif
