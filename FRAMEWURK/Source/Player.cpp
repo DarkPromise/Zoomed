e69a2142bc936bfcf85102128e3e744bbfe237dc
DarkPromise <hazard7@live.com.sg>
2015-09-04 12:12:52 +0800

    Changed Ahmad's Interaction Text

--- FRAMEWURK/Source/Player.cpp
+++ FRAMEWURK/Source/Player.cpp
@@ -428,7 +428,8 @@ void Player::Interact(double dt, World* currentWorld, std::vector<std::vector<in
 							this->setPosition(Vector3(Level3SpawnPointX, Level3SpawnPointY, 0));
 
 							theModel->getTextBox()->inText = true;
-							theModel->getTextBox()->setParagraph(1);
+							theModel->getTextBox()->setEpisode(3);
+							theModel->getTextBox()->setParagraph(0);
 						}
 						else if((controls.use) && ((m_playerPos.y == MainMenuNight4PositionY) && (m_playerPos.x  == MainMenuNight4PositionX)))
 						{
